ff085ddf3f104ccd97109fa584102a06dba6ebfb
aimankhan97 <ak02888@st.habib.edu.pk>
2019-03-11 19:17:15 +0500

    Add files via upload

---
+++ matrixMult.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def multiplyMatrices(a, b):
+    zip_b = zip(*b)
+    zip_b = list(zip_b)
+    return [[sum(ele_a*ele_b for ele_a, ele_b in zip(row_a, col_b)) 
+             for col_b in zip_b] for row_a in a]
+
+
